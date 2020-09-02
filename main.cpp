f1bcd9db48ac335d6d4ccd54b21bc97164bedd6c
kevin <kevin.giles@g.austincc.edu>
2020-09-02 14:19:12 -0500

    added the code as instructed

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*********************
+Name: Kevin Giles
+Coding 01
+Purpose: This is a simple Hello World in C++.
+**********************/
+
+#include "main.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,13 @@
+/*********************
+Name: Kevin Giles
+Coding 01
+Purpose: This is a simple Hello World in C++.
+**********************/
+
+#ifndef HELLOWORLD_MAIN_H
+#define HELLOWORLD_MAIN_H
+
+#include <iostream>
+
+#endif //HELLOWORLD_MAIN_H
+
